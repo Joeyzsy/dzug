d733129c6f439c76352200d4ae370042eb12b56d
Joeyzsy <helena0211@126.com>
2023-08-12 21:05:14 +0800

    feat(message):support basic db operation

---
+++ app/message/infra/db/init.go
@@ -0,0 +1,25 @@
+package dal
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	//gormopentracing "gorm.io/plugin/opentracing"
+)
+
+var DB *gorm.DB
+
+// Init init DB
+func Init() {
+	var err error
+	dsn := "root:123456@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	//dsn := conf.Config.MySQLConfig.Host
+	DB, err = gorm.Open(mysql.Open(dsn),
+		&gorm.Config{
+			PrepareStmt:            true,
+			SkipDefaultTransaction: true,
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ app/message/infra/db/orm.go
@@ -0,0 +1,22 @@
+package dal
+
+import (
+	"context"
+)
+
+func CreateMessage(ctx context.Context, message *Message) error {
+
+	err := DB.WithContext(ctx).Create(&message).Error
+
+	return err
+}
+
+func GetMessageList(ctx context.Context, userId int64, toUserID int64, latestTime int64) ([]*Message, error) {
+	var messages []*Message
+	err := DB.WithContext(ctx).Where("from_user_id = ? AND to_user_id = ? AND create_time >= ?",
+		userId, toUserID, latestTime).Find(&messages).Error
+	if err != nil {
+		return nil, err
+	}
+	return messages, nil
+}
